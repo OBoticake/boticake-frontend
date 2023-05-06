d55d5e9def86ee62aec3d72497edd492e6075de8
paulaandrezza <paulaandrezza25@gmail.com>
2023-05-06 18:38:01 -0300

    Add margim bottom

--- components/CustomTabs.js
+++ components/CustomTabs.js
@@ -47,7 +47,7 @@ export const CustomTabs = ({ tabs }) => {
   };
 
   return (
-    <Box sx={{ width: '100%' }}>
+    <Box sx={{ width: '100%', mb: 8 }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs value={value} onChange={handleChange} aria-label="basic tabs example" >
           {tabs.map((tab) => (
