ce67467620a90ceea54da3ef0236c2b0543adf60
paulaandrezza <paulaandrezza25@gmail.com>
2023-05-08 15:00:59 -0300

    change theme

--- pages/_app.js
+++ pages/_app.js
@@ -19,7 +19,7 @@ export default function MyApp(props) {
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      <ThemeProvider theme={darkTheme}>
+      <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...pageProps} />
