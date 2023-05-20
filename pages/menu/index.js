5a3ca767c27ca00be295e6f06b93f0f8c3d08822
paulaandrezza <paulaandrezza25@gmail.com>
2023-05-19 21:00:01 -0300

    change the directory of menu

--- pages/menu/index.js
+++ pages/menu/index.js
@@ -1,16 +1,16 @@
 import * as React from 'react';
 import { Container, Box } from '@mui/material';
-import { Header } from '../components/Header.js';
-import { CustomTabs } from '../components/CustomTabs.js';
-import { NavigationBar } from '../components/NavigationBar.js';
+import { Hero } from '../../components/Hero.js';
+import { CustomTabs } from '../../components/CustomTabs.js';
+import { NavigationBar } from '../../components/NavigationBar.js';
 
-import { aboutObj, tabsObj } from '../utils/Data.js';
+import { aboutObj, tabsObj } from '../../utils/Data.js';
 
 export default function Menu() {
   return (
     <Container maxWidth="md">
       <Box>
-        <Header {...aboutObj} />
+        <Hero {...aboutObj} />
         <CustomTabs {...tabsObj} />
         <NavigationBar />
       </Box>
